911f287ad271185dc3359dd083796996105cb94a
Fabian Jaimes <fabianjaimes84@hotmail.com>
2026-06-27 00:53:03 -0500

    Implementar estructura básica de la aplicación y agregar funcionalidad de navegación

--- src/main.js
+++ src/main.js
@@ -1,2 +1,61 @@
-console.log(API_KEY);
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },   
+    params: {
+        'api_key': API_KEY,
+    }
+});
+
+// <!--Imagenes en tendencias-->
+async function getTrendingMoviesPreview() {
+    const { data } = await api('trending/movie/day');    
+    
+    const movies = data.results;
+    console.log({ data, movies });
+
+    movies.forEach(movie => {
+
+        const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList');
+
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+        
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+    
+        movieContainer.appendChild(movieImg);
+        trendingMoviesPreviewList.appendChild(movieContainer);
+    });
+}
+
+// <!--Listado de Categorias-->
+async function getCategoriesPreview() {
+    const { data } = await api('genre/movie/list');
+    
+    const categories = data.genres;
+    console.log({ data, categories });
+
+    categories.forEach(category => {
+
+        const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+        
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id' + category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        categoriesPreviewList.appendChild(categoryContainer);
+    });
+}
+ 
+
 
